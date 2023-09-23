29402d02b2d88210b92ae9dabd571f815c3875b9
doroteablanco <130961780+doroteablanco@users.noreply.github.com>
2023-09-24 02:58:32 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        logging = true
+        basic.showIcon(IconNames.Yes)
+    } else {
+        logging = false
+        basic.showIcon(IconNames.No)
+    }
+})
 datalogger.onLogFull(function () {
     logging = false
     basic.showLeds(`
@@ -8,18 +17,11 @@ datalogger.onLogFull(function () {
         # # # # #
         `)
 })
-input.onButtonPressed(Button.A, function () {
-    logging = true
-    basic.showIcon(IconNames.Yes)
-})
-input.onButtonPressed(Button.B, function () {
-    logging = false
-    basic.showIcon(IconNames.No)
-})
 let logging = false
+radio.setGroup(21)
 let display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 logging = false
-datalogger.setColumnTitles("angle")
+datalogger.setColumnTitles("Angle Value")
 basic.showIcon(IconNames.No)
 basic.forever(function () {
     display.show(pins.map(
@@ -29,8 +31,21 @@ basic.forever(function () {
     0,
     180
     ))
+    radio.sendNumber(pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    180
+    ))
     if (logging) {
         led.toggle(4, 4)
-        datalogger.log(datalogger.createCV("angle", display))
+        datalogger.log(datalogger.createCV("Angle Value", pins.map(
+        pins.analogReadPin(AnalogPin.P0),
+        0,
+        1023,
+        0,
+        180
+        )))
     }
 })
