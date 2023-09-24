a6fbfaf19bdbdafe93d6bc51e4b8a6cc57016fef
doroteablanco <130961780+doroteablanco@users.noreply.github.com>
2023-09-24 14:52:30 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,35 +17,23 @@ datalogger.onLogFull(function () {
         # # # # #
         `)
 })
+radio.onReceivedValue(function (name, value) {
+    let angle = 0
+    if (angle) {
+        display.show(angle)
+        if (logging) {
+            led.toggle(4, 4)
+            datalogger.log(datalogger.createCV("Angle Value - Actuator 1", angle))
+        }
+    }
+})
 let logging = false
+let display: grove.TM1637 = null
 radio.setGroup(21)
-let display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
+display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 logging = false
-datalogger.setColumnTitles("Angle Value")
+datalogger.setColumnTitles("Angle Value - Actuator 1")
 basic.showIcon(IconNames.No)
 basic.forever(function () {
-    display.show(pins.map(
-    pins.analogReadPin(AnalogPin.P0),
-    0,
-    1023,
-    0,
-    180
-    ))
-    radio.sendNumber(pins.map(
-    pins.analogReadPin(AnalogPin.P0),
-    0,
-    1023,
-    0,
-    180
-    ))
-    if (logging) {
-        led.toggle(4, 4)
-        datalogger.log(datalogger.createCV("Angle Value", pins.map(
-        pins.analogReadPin(AnalogPin.P0),
-        0,
-        1023,
-        0,
-        180
-        )))
-    }
+	
 })
